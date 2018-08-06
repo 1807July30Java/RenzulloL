a5d0c3b07ef70cb68b369a25f67e3bbdef5689ed
fearlesspandas <landonrenzullo@gmaill.com>
2018-08-06 19:03:30 -0400

    fixed incorrect search algorithm

--- MinimumMutations.java
+++ MinimumMutations.java
@@ -23,15 +23,28 @@ public class MinimumMutations{
     }
 
     public int findMinDistance(){
-        ArrayDeque<String> Q = new ArrayDeque<String>();
-        String current =this.start;
+        ArrayDeque<String> Qcurrent = new ArrayDeque<String>();
+        ArrayDeque<String> Qnext = new ArrayDeque<String>();
+        HashMap<String,Boolean> H = new HashMap<String,Boolean>();
         int depth = 0;
-        while(this.end != current){
-            ArrayList<String> N = getNeighbors(current);
-            for(String n:N){Q.push(n);}
-            current = Q.pop();
+        int nvisited = 0;
+        Qcurrent.push(this.start);
+        if(this.start.equals(this.end)){return 0;}
+        while(!Qcurrent.isEmpty()){
+            Qnext.clear();
             depth++;
+            while(!Qcurrent.isEmpty()){//adds layer
+              ArrayList<String> N = getNeighbors(Qcurrent.pop());
+              for(String n:N){//adds unseen neighbors to next layer
+                  if(n.equals(this.end)){return depth;}
+                  if(!H.containsKey(n)){
+                     H.put(n,true);
+                     Qnext.push(n);
+                    }
+                }
+            }
+            Qcurrent = Qnext.clone();
         }
-      return depth;
+      return -1;
     }
 }
